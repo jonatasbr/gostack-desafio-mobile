12136bbf837d3c53095dd3b4c2c14bb1a8dc4609
Jonatas Braz <jonatasbraz@gmail.com>
2019-10-23 00:58:31 -0300

    project: limpeza de código

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import Teste from '~/Teste';
 
 // import { Container } from './styles';
 
 export default function src() {
   return (
     <View>
-      <Text>
-        <Teste />
-      </Text>
+      <Text>TESTE</Text>
     </View>
   );
 }
