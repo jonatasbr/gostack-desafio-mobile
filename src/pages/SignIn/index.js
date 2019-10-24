ee0555c11a11cba64bd32357cccb29797ae04405
Jonatas Braz <jonatasbraz@gmail.com>
2019-10-24 02:00:12 -0300

    feat: page SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
+import PropTypes from 'prop-types';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Button from '~/components/Button';
-import Input from '~/components/Input';
-// import {Container} from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
-      <Input
-        style={{marginTop: 30}}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
+      <Container>
+        <Image source={logo} />
 
-      <Button>Entrar</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite seu e-mail"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
